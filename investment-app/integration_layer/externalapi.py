fcd292f29c3510af61b81928c02f2ec922c4c8a5
LoganOBerk <146039828+LoganOBerk@users.noreply.github.com>
2026-03-28 22:01:35 -0400

    finished the visualizer and externalapi documentation

--- investment-app/integration_layer/externalapi.py
+++ investment-app/integration_layer/externalapi.py
@@ -1,23 +1,35 @@
 import yfinance as yf
 
 
-# PURPOSE: To provide a clean abstraction interface for retrieving live stock data
+# PURPOSE:
+#   -ExternalApi provides a external finance fetching abstraction
+#   -provides functionality related to fetching live stock data
 class ExternalApi:
 
-    # INPUT: string representing a stock ticker
-    # OUTPUT: int representing stock price
-    # PRECONDITION: stock ticker is a real ticker
-    # POSTCONDITION: Non
+    # INPUT:
+    #   -ticker(str); a stock ticker symbol
+    # OUTPUT:
+    #   -price(int); a live stock price
+    # PRECONDITION:
+    #   -ticker; ticker symbol is a real ticker in open market
+    # POSTCONDITION:
+    #   -price; corresponding live ticker price has been fetched from yfinance
+    # RAISES: None
     @staticmethod
     def get_stock_price(ticker : str) -> int:
         # TODO: Pull stock price data given a ticker
         pass
 
 
-    # INPUT: string representing a stock ticker
-    # OUTPUT: bool representing if the ticker exists
-    # PRECONDITION: stock ticker is a valid ticker format
-    # POSTCONDITION: None
+    # INPUT:
+    #   -ticker(str); a stock ticker symbol 
+    # OUTPUT:
+    #   -exist(bool); True or False
+    # PRECONDITION:
+    #   -ticker; matches basic format with <= 5 capital chars
+    # POSTCONDITION:
+    #   -exist; ticker is identified as being a real(True) or fake(False)
+    # RAISES: None
     @staticmethod
     def does_ticker_exist(ticker : str) -> bool:
         # TODO: Call the api to identify if the ticker actually exists
--- investment-app/interface_layer/visualizer.py
+++ investment-app/interface_layer/visualizer.py
@@ -2,15 +2,21 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-# PURPOSE: To provide a clean abstraction for visualizing stock data in a structured way
+# PURPOSE:
+#   -Visualizer provides a data visualization abstraction
+#   -provides a isolated layer that allows for construction of data charts
 class Visualizer:
     def __init__(self):
         self.fig, self.ax = plt.subplots()
         
-    # INPUT: a list of dicts with key string and value either string or int
+    # INPUT:
+    #   -portfolio_data(list[dict[str|int]]); formatted portfolio data
     # OUTPUT: None
-    # PRECONDITION: portfolio data is all valid, existing, and readable by pandas
-    # POSTCONDITION: a pie chart is displayed representing stock distributions without blocking program
+    # PRECONDITION:
+    #   -portfolio_data; non-empty, each dict contains 'ticker'(str) and 'quantity'(int)
+    # POSTCONDITION:
+    #   -process; a pie chart is displayed with stock data distributions does not block program execution
+    # RAISES: None
     def display_pie_chart(self, portfolio_data : list[dict[str, str | int]]) -> None:
 
 
@@ -23,8 +29,11 @@ class Visualizer:
     
     # INPUT: None
     # OUTPUT: None
-    # PRECONDITION: A chart window is currently open and displayed
-    # POSTCONDITION: The chart window is properly closed
+    # PRECONDITION:
+    #   -process; current process has a chart displayed
+    # POSTCONDITION:
+    #   -process; chart is closed
+    # RAISES: None
     def close_chart(self) -> None:
         plt.close(self.fig)
             
